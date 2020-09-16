26d07895ccc53758fa9fdf4c17a77be2d08b28b0
ratrap44 <62080086+ratrap44@users.noreply.github.com>
2020-09-16 17:37:34 -0400

    lab02

---
+++ AGGP225Lab02/Assets/Scripts/CardUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardUI : MonoBehaviour
+{
+    RPSC cardDef;
+    Image Art;
+    
+
+    public void Start()
+    {
+        if(cardDef)
+        {
+            SetupCard();
+        }
+    }
+
+    public void SetupCard()
+    {
+        
+    }
+}
---
+++ AGGP225Lab02/Assets/Scripts/RPSC.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+
+public enum ChallangeResult
+{
+    Win,
+    Lose,
+    Draw
+
+}
+
+[CreateAssetMenu(fileName = "RPS card", menuName = "card")]
+public class RPSC : ScriptableObject
+{
+    
+     public enum Type
+     {
+        Rock,
+        Paper,
+        Scissors,
+        Water,
+        Air,
+        Sponge,
+        fire
+
+     }
+    public Type cardtype;
+    public List<Type> loseto;
+    public List<Type> winto;
+
+    public ChallangeResult PerformChallenge(RPSC card)
+    {
+        if (cardtype == card.cardtype)
+        {
+            return ChallangeResult.Draw;
+        }
+        if (cardtype == Type.Rock && card.cardtype == Type.Paper)
+        {
+
+        }
+    }
+  
+ 
+}
+
